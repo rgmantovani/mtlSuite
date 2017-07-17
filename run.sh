b7048969861a4b545d837c391245d582a3190c98
Rafael G. Mantovani <rgmantovani@gmail.com>
2017-07-16 23:12:32 -0300

    fixing bash script

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ do
         for rep in $(seq 1 10);
         do
           R CMD BATCH --no-save --no-restore '--args' --datafile="$data" --algo="$algo" --norm="$norm" --feat.sel="$feat" -seed="$rep" \
-            mainMTL.R out_"$data"_"$algo"_"$norm"_"feat.sel"_"$rep".log &
+            mainMTL.R out_"$data"_"$algo"_"$norm"_"$feat"_"$rep".log &
           PIDS[$rep]=$!
         done
         for k in $(seq 1 10);
